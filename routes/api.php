83c9833021a7418edc8e66d9875f0f13dea02a6d
Sutrisno <masutrisn021@gmail.com>
2024-05-30 19:07:31 +0700

    feat:making rest api login& register  laravel 11 using jwt

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
+Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
